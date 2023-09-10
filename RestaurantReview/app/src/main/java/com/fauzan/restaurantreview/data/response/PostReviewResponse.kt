42980c07d2e064c2865302f15b64e7a2ff4a964b
Fauzan Radji <triputrafauzan@gmail.com>
2023-09-10 20:51:46 +0800

    background thread dan networking: latihan networking dengan retrofit

---
+++ RestaurantReview/app/src/main/java/com/fauzan/restaurantreview/data/response/PostReviewResponse.kt
@@ -0,0 +1,15 @@
+package com.fauzan.restaurantreview.data.response
+
+import com.google.gson.annotations.SerializedName
+
+data class PostReviewResponse(
+
+	@field:SerializedName("customerReviews")
+	val customerReviews: List<CustomerReviewsItem>,
+
+	@field:SerializedName("error")
+	val error: Boolean,
+
+	@field:SerializedName("message")
+	val message: String
+)
---
+++ RestaurantReview/app/src/main/java/com/fauzan/restaurantreview/ui/ReviewAdapter.kt
@@ -0,0 +1,43 @@
+package com.fauzan.restaurantreview.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.fauzan.restaurantreview.data.response.CustomerReviewsItem
+import com.fauzan.restaurantreview.databinding.ItemReviewBinding
+
+class ReviewAdapter : ListAdapter<CustomerReviewsItem, ReviewAdapter.MyViewHolder>(DIFF_CALLBACK) {
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<CustomerReviewsItem>() {
+            override fun areItemsTheSame(
+                oldItem: CustomerReviewsItem,
+                newItem: CustomerReviewsItem
+            ): Boolean = oldItem == newItem
+
+            override fun areContentsTheSame(
+                oldItem: CustomerReviewsItem,
+                newItem: CustomerReviewsItem
+            ): Boolean = oldItem == newItem
+        }
+    }
+
+    class MyViewHolder(private val binding: ItemReviewBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(review: CustomerReviewsItem) {
+            binding.tvItem.text = "${review.review}\n- ${review.name}"
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = ItemReviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val review = getItem(position)
+        holder.bind(review)
+    }
+
+}
