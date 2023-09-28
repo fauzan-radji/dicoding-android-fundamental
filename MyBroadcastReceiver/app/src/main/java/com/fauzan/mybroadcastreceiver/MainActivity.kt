9d2be66abba856cfa62aaaa3ac4c8902cebe2daa
Fauzan Radji <triputrafauzan@gmail.com>
2023-09-29 01:28:56 +0800

    background task & scheduler: latihan broadcastreceiver dari custom event

--- MyBroadcastReceiver/app/src/main/java/com/fauzan/mybroadcastreceiver/MainActivity.kt
+++ MyBroadcastReceiver/app/src/main/java/com/fauzan/mybroadcastreceiver/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.fauzan.mybroadcastreceiver
 
 import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
@@ -10,6 +16,11 @@ import com.fauzan.mybroadcastreceiver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    companion object {
+        const val ACTION_DOWNLOAD_STATUS = "download_status"
+    }
+
+    private lateinit var downloadReceiver: BroadcastReceiver
     private var binding: ActivityMainBinding? = null
 
     private var requestPermissionLauncher = registerForActivityResult(RequestPermission()) { isGranted ->
@@ -30,16 +41,32 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(binding?.root)
 
         binding?.btnPermission?.setOnClickListener(this)
+        binding?.btnDownload?.setOnClickListener(this)
+
+        downloadReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent) {
+                Toast.makeText(context, "Download Selesai", Toast.LENGTH_SHORT).show()
+            }
+        }
+        val downloadIntentFilter = IntentFilter(ACTION_DOWNLOAD_STATUS)
+        registerReceiver(downloadReceiver, downloadIntentFilter)
     }
 
     override fun onClick(v: View) {
         when (v.id) {
             R.id.btn_permission -> requestPermissionLauncher.launch(Manifest.permission.RECEIVE_SMS)
+            R.id.btn_download -> {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    val notifyFinishIntent = Intent().setAction(ACTION_DOWNLOAD_STATUS)
+                    sendBroadcast(notifyFinishIntent)
+                }, 3000)
+            }
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        unregisterReceiver(downloadReceiver)
         binding = null
     }
 }
